abdd60977cbd12d3cd931d0e6e07cc0a70138c95
Angular CLI <angular-cli@angular.io>
2018-07-19 20:26:40 +0200

    chore: initial commit from @angular/cli

---
+++ src/assets/api/get.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-type:application/json');
+$solution=[];
+$arrayFromCSV=array();
+$arrayFromCSV =  array_map('str_getcsv', file('Data for Test - Frontend Developer Pumox GmbH.csv'));
+foreach ($arrayFromCSV as $key => $value){
+    $array=explode(';',$value[0]);
+    array_push($solution, $array);
+}
+$return=json_encode($solution,JSON_FORCE_OBJECT);
+ echo $return ;
+?>
